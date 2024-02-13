eb60572933c88f44e2d71b5d8d284734953a6801
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-02-13 15:31:44 +0530

    start

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-full h-fit px-2 py-2 p-2 bg-blue-600 text-white">
+      <h1 className='font-bold text-center text-[25px] '>Projects</h1>
+    </div>
+  );
+}
+
+export default App;
